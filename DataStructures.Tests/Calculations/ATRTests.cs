1cb951d6fbc9d01365fb07e7e0346378acfd4e75
Benjlr <roberts.benjamin.luke@gmail.com>
2020-11-20 22:39:31 +1100

    REFACTOR

--- DataStructures.Tests/Calculations/ATRTests.cs
+++ DataStructures.Tests/Calculations/ATRTests.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using DataStructures.PriceAlgorithms;
 using Xunit;
 
 namespace DataStructures.Tests.Calculations
 {
     public class ATRTests
     {
+        private SessionData[] myArray = new SessionData[]
+        {
+            new SessionData(new DateTime(),12,5,6,4,5  ), 
+            new SessionData(new DateTime(),12,10,11,9,10  ), 
+            new SessionData(new DateTime(),12,11,12,10,11  ), 
+            new SessionData(new DateTime(),12,2,2,2,2  ), 
+            new SessionData(new DateTime(),12,3,4,3,3  ),
+            new SessionData(new DateTime(),12,3,5,3,4  ),
+            new SessionData(new DateTime(),12,4,20,4,15  ),
+            new SessionData(new DateTime(),12,15,18,14,14  ),
+            new SessionData(new DateTime(),12,14,16,12,13  ),
+            new SessionData(new DateTime(),9,9,10,6,7  ),
+        };
+
+
         [Fact]
         private void ShouldCalculateATRCorrectly() {
-            Assert.True(false);
+            Assert.Equal(new List<double>(){ 2, 2.8, 2.6399999999999997, 3.912, 3.5296, 3.2236800000000003, 5.778944, 5.4231552, 5.13852416, 5.510819328 },
+                AverageTrueRange.Calculate(myArray.ToList(), 5));
+        }
+
+        [Fact]
+        private void ShouldCalculateATRPCCorrectly() {
+            Assert.Equal(new List<double>() { 2, 2.8, 2.6399999999999997, 3.912, 3.5296 },
+                AverageTrueRange.CalculateATRPC(myArray.ToList(),5,3));
         }
     }
 }
--- PriceDataStructures/PriceAlgorithms/AverageTrueRange.cs
+++ PriceDataStructures/PriceAlgorithms/AverageTrueRange.cs
@@ -8,14 +8,16 @@ namespace DataStructures.PriceAlgorithms
     {
         public static List<double> Calculate(List<SessionData> input, int period = 20) {
             var atr = new List<double>();
-
-            var trueRangeVals = new List<double> { input.First().High - input.First().Low, Math.Abs(input.First().High - input.First().Close), Math.Abs(input.First().Low - input.First().Close) };
-            atr.Add(trueRangeVals.Max());
+            atr.Add(input.First().High - input.First().Low);
 
             for (var i = 1; i < input.Count; i++) {
-                trueRangeVals = new List<double> { input[i].High - input[i].Low, Math.Abs(input[i].High - input[i - 1].Close), Math.Abs(input[i].Low - input[i - 1].Close) };
-                var tr = trueRangeVals.Max();
-                atr.Add((atr.Last() * (period - 1.00) + tr) / period);
+                var trueRangeVals = new List<double>
+                {
+                    input[i].High - input[i].Low, 
+                    Math.Abs(input[i].High - input[i - 1].Close), 
+                    Math.Abs(input[i].Low - input[i - 1].Close)
+                };
+                atr.Add(((atr.Last() * (period - 1.0)) + trueRangeVals.Max()) / period);
             }
 
             return atr;
@@ -26,8 +28,8 @@ namespace DataStructures.PriceAlgorithms
             var atrPC = new List<double>();
 
             for (int i = 0; i < atr.Count; i++) {
-                if (i > ATRPCLB + 1) {
-                    var lastTwenty = atr.Skip(Math.Max(0, i - ATRPCLB)).Take(ATRPCLB + 1).ToList();
+                if (i > ATRPCLB ) {
+                    var lastTwenty = atr.Skip(Math.Max(0, i - ATRPCLB-1)).Take(ATRPCLB+1).ToList();
 
                     var last = lastTwenty.Last();
                     var Min = lastTwenty.Min();
@@ -37,7 +39,7 @@ namespace DataStructures.PriceAlgorithms
                     if (curr < 0.1) curr = 0;
                     atrPC.Add(curr);
                 }
-                else atrPC.Add(0);
+                else atrPC.Add(1);
             }
 
             return atrPC;
