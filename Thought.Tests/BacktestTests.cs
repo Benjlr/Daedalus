f9ed93a60ced08113e9df248196df0c2d0f1d7b0
Benjlr <roberts.benjamin.luke@gmail.com>
2020-12-26 09:23:40 +1100

    refactor

--- Thought.Tests/BacktestTests.cs
+++ Thought.Tests/BacktestTests.cs
@@ -16,68 +16,29 @@ namespace Thought.Tests
         {
         }
 
-        public List<long> ExecutionDates{get;set;} = new List<long>();
-
-  
-
-        public override List<Trade> RunBackTest(StrategyExecuter exec)
-        {
-            return base.RunBackTest(exec);
-        }
-
-        public override List<Trade> RunBackTestByDates(List<StrategyExecuter> exec)
-        {
-            return base.RunBackTestByDates(exec);
-        }
+        public List<DateTime> ExecutionDates{get;set;} = new List<DateTime>();
 
         protected override void IterateThroughMarkets(){
-            ExecutionDates.Add(laggardDate);
+            ExecutionDates.Add(new DateTime(laggardDate));
             base.IterateThroughMarkets();
-            return;
         }
     }
 
     public class BackTestFixture
     {
         public Backtest BackTest { get; private set; }
+        public BackTestSpy BackTestSpyOne { get; private set; }
+        public BackTestSpy BackTestSpyTwo { get; private set; }
+        public BackTestSpy BackTestSpyThree { get; private set; }
         public List<Trade> TradesGenerated { get; private set; }
 
-        public List<Trade> Trades = new List<Trade>()
-        {
-            new Trade(new DatedResult[]
-            {
-                new DatedResult(new DateTime(1,1,1,1,1,1).Ticks, 0.1,-0.1 ),
-                new DatedResult(new DateTime(1,1,10,1,1,1).Ticks, 0.15,-0.15 ),
-                new DatedResult(new DateTime(1,1,20,1,1,1).Ticks, 0.2,-0.15 ),
-                new DatedResult(new DateTime(1,1,30,1,1,1).Ticks, 0.25,-0.2 ),
-            }, 0),
-            new Trade(new DatedResult[]
-            {
-                new DatedResult(new DateTime(1,1,1,1,1,1).Ticks, 0.1,-0.05 ),
-                new DatedResult(new DateTime(1,1,5,1,1,1).Ticks, 0.05,-0.05 ),
-                new DatedResult(new DateTime(1,1,10,1,1,1).Ticks, -0.05,-0.15 ),
-                new DatedResult(new DateTime(1,1,15,1,1,1).Ticks, 0.05,-0.15 ),
-                new DatedResult(new DateTime(1,1,20,1,1,1).Ticks, 0.1,-0.15 ),
-            }, 0),
-            new Trade(new DatedResult[]
-            {
-                new DatedResult(new DateTime(1,1,1,1,1,1).Ticks, 0.1,-0.05 ),
-                new DatedResult(new DateTime(1,1,2,1,1,1).Ticks, 0.05,-0.05 ),
-                new DatedResult(new DateTime(1,1,3,1,1,1).Ticks, -0.05,-0.1 ),
-                new DatedResult(new DateTime(1,1,4,1,1,1).Ticks, 0.05,-0.11 ),
-                  new DatedResult(new DateTime(1, 1, 5,1,1,1).Ticks, 0.08, -0.12),
-                  new DatedResult(new DateTime(1, 1, 6,1,1,1).Ticks, 0.12, -0.13),
-                  new DatedResult(new DateTime(1, 1, 7,1,1,1).Ticks, 0.14, -0.14),
-                  new DatedResult(new DateTime(1, 1, 8,1,1,1).Ticks, 0.16, -0.15),
-                  new DatedResult(new DateTime(1, 1, 9,1,1,1).Ticks, 0.1, -0.16),
-                  new DatedResult(new DateTime(1, 1, 10,1,1,1).Ticks, 0.1, -0.2),
-            }, 0),
-        };
+     
         private StaticStrategy _strat { get; set; }
         private StaticStrategy _stratForSpy { get; set; }
 
         public BackTestFixture() {
             GenerateGeneraleBackTest();
+            GenerateBacktestSpies();
         }
 
         private void GenerateGeneraleBackTest() {
@@ -103,6 +64,104 @@ namespace Thought.Tests
             TradesGenerated = BackTest.RunBackTest(new LongStrategyExecuter(true));
         }
 
+        private void GenerateBacktestSpies() {
+            SpyOne();
+            SpyTwo();
+            SpyThree();
+        }
+
+        private void SpyOne() {
+            var minuteMarket = new Market(new RandomBars(new TimeSpan(0, 5, 0)).GenerateRandomMarket(400), "shortMarket");
+            var fiveminuteMarket = new Market(new RandomBars(new TimeSpan(0, 10, 0)).GenerateRandomMarket(80), "medMarket");
+            var tenminuteMarket = new Market(new RandomBars(new TimeSpan(0, 15, 0)).GenerateRandomMarket(40), "longMarket");
+
+            var rules = new IRuleSet[] { new DummyEntries(5, 1000), new DummyExits(5, 1000) };
+            var exits = ExitPrices.NoStopTarget();
+            var minuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, minuteMarket, new StaticStopTarget(exits));
+            var fiveminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, fiveminuteMarket, new StaticStopTarget(exits));
+            var tenminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, tenminuteMarket, new StaticStopTarget(exits));
+            var univers = new Universe();
+            univers.AddMarket(minuteMarket, minuteStrat);
+            univers.AddMarket(fiveminuteMarket, fiveminuteStrat);
+            univers.AddMarket(tenminuteMarket, tenminuteStrat);
+
+            BackTestSpyOne = new BackTestSpy(univers);
+        }
+        private void SpyTwo() {
+            var minuteMarket = new Market(new RandomBars(new TimeSpan(0, 5, 0)).GenerateRandomMarket(50), "shortMarket");
+            var fiveminuteMarket = new Market(new RandomBars(new TimeSpan(0, 19, 0)).GenerateRandomMarket(110), "medMarket");
+            var tenminuteMarket = new Market(new RandomBars(new TimeSpan(0, 35, 0)).GenerateRandomMarket(200), "longMarket");
+
+            var rules = new IRuleSet[] { new DummyEntries(5, 1000), new DummyExits(5, 1000) };
+            var exits = ExitPrices.NoStopTarget();
+            var minuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, minuteMarket, new StaticStopTarget(exits));
+            var fiveminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, fiveminuteMarket, new StaticStopTarget(exits));
+            var tenminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, tenminuteMarket, new StaticStopTarget(exits));
+            var univers = new Universe();
+            univers.AddMarket(minuteMarket, minuteStrat);
+            univers.AddMarket(fiveminuteMarket, fiveminuteStrat);
+            univers.AddMarket(tenminuteMarket, tenminuteStrat);
+
+            BackTestSpyTwo = new BackTestSpy(univers);
+        }
+        private void SpyThree() {
+            var m1 = new Market(new RandomBars(new TimeSpan(123564)).GenerateRandomMarket(224), "shortMarket");
+            var m2 = new Market(new RandomBars(new TimeSpan(345)).GenerateRandomMarket(657), "medMarket");
+            var m3 = new Market(new RandomBars(new TimeSpan(3215644)).GenerateRandomMarket(150), "longMarket");
+            var m4 = new Market(new RandomBars(new TimeSpan(123354)).GenerateRandomMarket(700), "otherma");
+            var m5 = new Market(new RandomBars(new TimeSpan(4561)).GenerateRandomMarket(1000), "anotherM!!");
+
+            var rules = new IRuleSet[] { new DummyEntries(5, 1000), new DummyExits(5, 1000) };
+            var exits = ExitPrices.NoStopTarget();
+            var minuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, m1, new StaticStopTarget(exits));
+            var fiveminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, m2, new StaticStopTarget(exits));
+            var tenminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, m3, new StaticStopTarget(exits));
+            var fiftminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, m4, new StaticStopTarget(exits));
+            var twentminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, m5, new StaticStopTarget(exits));
+            var univers = new Universe();
+            univers.AddMarket(m1, minuteStrat);
+            univers.AddMarket(m2, fiveminuteStrat);
+            univers.AddMarket(m3, tenminuteStrat);
+            univers.AddMarket(m4, fiftminuteStrat);
+            univers.AddMarket(m5, twentminuteStrat);
+
+            BackTestSpyThree = new BackTestSpy(univers);
+        }
+
+        public List<Trade> GetTrades() {
+            return new List<Trade>()
+            {
+                new Trade(new DatedResult[]
+                {
+                    new DatedResult(new DateTime(1, 1, 1, 1, 1, 1).Ticks, 0.1, -0.1),
+                    new DatedResult(new DateTime(1, 1, 10, 1, 1, 1).Ticks, 0.15, -0.15),
+                    new DatedResult(new DateTime(1, 1, 20, 1, 1, 1).Ticks, 0.2, -0.15),
+                    new DatedResult(new DateTime(1, 1, 30, 1, 1, 1).Ticks, 0.25, -0.2),
+                }, 0),
+                new Trade(new DatedResult[]
+                {
+                    new DatedResult(new DateTime(1, 1, 1, 1, 1, 1).Ticks, 0.1, -0.05),
+                    new DatedResult(new DateTime(1, 1, 5, 1, 1, 1).Ticks, 0.05, -0.05),
+                    new DatedResult(new DateTime(1, 1, 10, 1, 1, 1).Ticks, -0.05, -0.15),
+                    new DatedResult(new DateTime(1, 1, 15, 1, 1, 1).Ticks, 0.05, -0.15),
+                    new DatedResult(new DateTime(1, 1, 20, 1, 1, 1).Ticks, 0.1, -0.15),
+                }, 0),
+                new Trade(new DatedResult[]
+                {
+                    new DatedResult(new DateTime(1, 1, 1, 1, 1, 1).Ticks, 0.1, -0.05),
+                    new DatedResult(new DateTime(1, 1, 2, 1, 1, 1).Ticks, 0.05, -0.05),
+                    new DatedResult(new DateTime(1, 1, 3, 1, 1, 1).Ticks, -0.05, -0.1),
+                    new DatedResult(new DateTime(1, 1, 4, 1, 1, 1).Ticks, 0.05, -0.11),
+                    new DatedResult(new DateTime(1, 1, 5, 1, 1, 1).Ticks, 0.08, -0.12),
+                    new DatedResult(new DateTime(1, 1, 6, 1, 1, 1).Ticks, 0.12, -0.13),
+                    new DatedResult(new DateTime(1, 1, 7, 1, 1, 1).Ticks, 0.14, -0.14),
+                    new DatedResult(new DateTime(1, 1, 8, 1, 1, 1).Ticks, 0.16, -0.15),
+                    new DatedResult(new DateTime(1, 1, 9, 1, 1, 1).Ticks, 0.1, -0.16),
+                    new DatedResult(new DateTime(1, 1, 10, 1, 1, 1).Ticks, 0.1, -0.2),
+                }, 0)
+            };
+
+        }
     }
 
     public class BacktestTests : IClassFixture<BackTestFixture>
@@ -115,7 +174,7 @@ namespace Thought.Tests
 
         [Fact]
         private void GeneratesResults() {
-            var results = _fixture.BackTest.ParseResults(TimeSpan.FromDays(1), _fixture.TradesGenerated);
+            var results = _fixture.BackTest.ResultsCollater.ParseResults(TimeSpan.FromDays(1), _fixture.TradesGenerated);
             Assert.True(results.Count > 0);
             foreach (var trade in results) {
                 Assert.True(trade.Return != 0);
@@ -125,7 +184,7 @@ namespace Thought.Tests
 
         [Fact]
         private void GeneratesResultsForDays() {
-            var results = _fixture.BackTest.ParseResults(TimeSpan.FromDays(1), _fixture.TradesGenerated);
+            var results = _fixture.BackTest.ResultsCollater.ParseResults(TimeSpan.FromDays(1), _fixture.TradesGenerated);
 
             Assert.True(results.Count > 0);
             Assert.False(results.All(x => x.Drawdown == 0));
@@ -138,7 +197,7 @@ namespace Thought.Tests
 
         [Fact]
         private void GeneratesResultsForHours() {
-            var results = _fixture.BackTest.ParseResults(TimeSpan.FromHours(1), _fixture.TradesGenerated);
+            var results = _fixture.BackTest.ResultsCollater.ParseResults(TimeSpan.FromHours(1), _fixture.TradesGenerated);
 
             Assert.True(results.Count > 0);
             Assert.False(results.All(x => x.Drawdown == 0));
@@ -151,7 +210,7 @@ namespace Thought.Tests
 
         [Fact]
         private void GeneratesResultsForArbitraryTicks() {
-            var results = _fixture.BackTest.ParseResults(TimeSpan.FromTicks(29556547847), _fixture.TradesGenerated);
+            var results = _fixture.BackTest.ResultsCollater.ParseResults(TimeSpan.FromTicks(29556547847), _fixture.TradesGenerated);
 
             Assert.True(results.Count > 0);
             Assert.False(results.All(x=>x.Drawdown == 0));
@@ -165,7 +224,7 @@ namespace Thought.Tests
 
         [Fact]
         private void ShouldGenerateResultsInCorrectOrder() {
-            var results = _fixture.BackTest.ParseResults(TimeSpan.FromDays(10), _fixture.Trades);
+            var results = _fixture.BackTest.ResultsCollater.ParseResults(TimeSpan.FromDays(10), _fixture.GetTrades());
             AssertDatedResult(new DatedResult(new DateTime(1, 1, 11).Ticks, 0.15 - 0.05 + 0.1, (-0.3 + -0.2)/3), results[0]);
             AssertDatedResult(new DatedResult(new DateTime(1, 1, 21).Ticks, 0.2 + 0.1, -0.15), results[1]);
             AssertDatedResult(new DatedResult(new DateTime(1, 1, 31).Ticks, 0.25, -0.2), results[2]);
@@ -173,7 +232,7 @@ namespace Thought.Tests
 
         [Fact]
         private void ShouldGenerateResultsInCorrectOrderOnSmallScale() {
-            var results = _fixture.BackTest.ParseResults(TimeSpan.FromDays(5), _fixture.Trades);
+            var results = _fixture.BackTest.ResultsCollater.ParseResults(TimeSpan.FromDays(5), _fixture.GetTrades());
             AssertDatedResult(new DatedResult(new DateTime(1, 1, 6).Ticks, 0.1 + 0.05 + 0.08, (-0.1-0.05 -0.12)/3), results[0]);
             AssertDatedResult(new DatedResult(new DateTime(1, 1, 11).Ticks, 0.15 - 0.05 + 0.1, (-0.15-0.15 -0.2)/3), results[1]);
             AssertDatedResult(new DatedResult(new DateTime(1, 1, 16).Ticks, 0.15 + 0.05, (-0.15-0.15 )/2), results[2]);
@@ -184,26 +243,29 @@ namespace Thought.Tests
 
         [Fact]
         private void ShouldExecuteBackTestsByDates(){
-            var minuteMarket = new Market(new RandomBars(new TimeSpan(0,1,0)).GenerateRandomMarket(400),"shortMarket");
-            var fiveminuteMarket = new Market(new RandomBars(new TimeSpan(0,5,0)).GenerateRandomMarket(80),"medMarket");
-            var tenminuteMarket = new Market(new RandomBars(new TimeSpan(0,10,0)).GenerateRandomMarket(40),"longMarket");
+            var trades = _fixture.BackTestSpyOne.RunBackTestByDates(
+                new LongStrategyExecuter(false));
+            Assert.Equal(520/5, trades.Count);
+            for(int i =1; i < _fixture.BackTestSpyOne.ExecutionDates.Count; i++){
+                Assert.True(_fixture.BackTestSpyOne.ExecutionDates[i-1] <= _fixture.BackTestSpyOne.ExecutionDates[i]);
+            }
+        }
 
-            var rules = new IRuleSet[]{new DummyEntries(5,1000), new DummyExits(5,1000)};
-            var exits = ExitPrices.NoStopTarget();
-            var minuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, minuteMarket, new StaticStopTarget(exits));
-            var fiveminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, fiveminuteMarket, new StaticStopTarget(exits));
-            var tenminuteStrat = new StaticStrategy.StrategyBuilder().CreateStrategy(rules, tenminuteMarket, new StaticStopTarget(exits));
-            var univers = new Universe();
-            univers.AddMarket(minuteMarket, minuteStrat);
-            univers.AddMarket(fiveminuteMarket, fiveminuteStrat);
-            univers.AddMarket(tenminuteMarket, tenminuteStrat);
+        [Fact]
+        private void ShouldExecuteBackTestsByOutOfSyncDates() {
+            var trades = _fixture.BackTestSpyTwo.RunBackTestByDates(
+                new LongStrategyExecuter(false));
+            Assert.Equal( 360/5, trades.Count);
+            for (int i = 1; i < _fixture.BackTestSpyTwo.ExecutionDates.Count; i++)
+                Assert.True(_fixture.BackTestSpyTwo.ExecutionDates[i - 1] <= _fixture.BackTestSpyTwo.ExecutionDates[i]);
+        }
 
-            var backtester = new BackTestSpy(univers);
-            var trades = backtester.RunBackTestByDates(new List<StrategyExecuter>()
-            {new LongStrategyExecuter(false), new LongStrategyExecuter(false), new LongStrategyExecuter(false)});
-            Assert.True(trades.Count > 0);
-            for(int i =1; i < backtester.ExecutionDates.Count; i++){
-                Assert.True(backtester.ExecutionDates[i-1] <= backtester.ExecutionDates[i]);
+        [Fact]
+        private void ShouldExecuteBackTestsByRandomishDates() {
+            var trades = _fixture.BackTestSpyThree.RunBackTestByDates(new LongStrategyExecuter(false));
+            Assert.Equal((2731 / 5)+1, trades.Count);
+            for (int i = 1; i < _fixture.BackTestSpyThree.ExecutionDates.Count; i++) {
+                Assert.True(_fixture.BackTestSpyThree.ExecutionDates[i - 1] <= _fixture.BackTestSpyThree.ExecutionDates[i]);
             }
         }
 
--- Thought/Backtest.cs
+++ Thought/Backtest.cs
@@ -5,65 +5,50 @@ using System.Linq;
 
 namespace Thought
 {
-    public class Backtest
+    public class ResultsCollater
     {
-        public Universe Markets { get; }
-
-        public Backtest(Universe markets) {
-            Markets = markets;
-        }
-
-        public virtual List<Trade> RunBackTest(StrategyExecuter exec) {
-            var results = new List<Trade>();
-            foreach (var element in Markets.Elements) {
-                exec.Init(element);
-                results.AddRange(exec.ExecuteAll());
-            }
-
-            return results;
-        }
-
-        public virtual List<Trade> RunBackTestByDates(List<StrategyExecuter> exec){
-            results = new List<Trade>();
-            counters = new List<int>();
-            execs = exec;
-            laggardDate = new DateTime(3000,01,01).Ticks;
-            for(int i = 0; i < Markets.Elements.Count ; i++){
-                if(Markets.Elements[i].MarketData.PriceData[0].Open.Ticks < laggardDate)
-                    laggardDate =Markets.Elements[i].MarketData.PriceData[0].Open.Ticks;
-                execs[^1].Init(Markets.Elements[i]);
-                counters.Add(0);
-            }
-            IterateThroughMarkets();
-            return results;
-        }
-
-        List<Trade> results {get;set;}
-        List<StrategyExecuter> execs {get;set;}
-        List<int> counters {get;set;}
-        protected long laggardDate {get;set;}
-        long newlaggardDate {get;set;}
-        protected virtual void IterateThroughMarkets(){
-            newlaggardDate = new DateTime(3000,01,01).Ticks;
-            for(int i = 0; i < Markets.Elements.Count ; i++){
-                if(counters[i] == Markets.Elements[i].MarketData.PriceData.Length) 
+        private List<Trade> _orderedTrades { get; set; }
+        private List<DatedResult> _categorisedResults { get; set; }
+        private long _totalSpan { get; set; }
+        
+        public List<DatedResult> ParseTrades(TimeSpan time, long i) {
+            var resultsTw = new List<DatedResult>();
+            foreach (var t in _orderedTrades) {
+                if (CheckRelevant(time, t, i))
                     continue;
-                if(Markets.Elements[i].MarketData.PriceData[counters[i]].Open.Ticks < laggardDate){
-                    results.AddRange(execs[i].ExecuteStep());
-                    counters[i]++;
-                    if(Markets.Elements[i].MarketData.PriceData[counters[i]].Open.Ticks < newlaggardDate)
-                        newlaggardDate =Markets.Elements[i].MarketData.PriceData[counters[i]].Open.Ticks;
-                }
+                AddRelevantResult(time, t, i, resultsTw);
             }
-            laggardDate = newlaggardDate;
-
+            return resultsTw;
         }
 
-        private List<Trade> _orderedTrades;
-        private List<DatedResult> _categorisedResults;
-        private long _totalSpan;
+        private void SumAndAdd(List<DatedResult> resultsTw, long date) {
+            var avgDD = GetDrawdown(resultsTw);
+            var sum = resultsTw.Sum(x => x.Return);
+            _categorisedResults.Add(new DatedResult(date, sum, avgDD));
+        }
 
-        public List<DatedResult> ParseResults(TimeSpan time, List<Trade> results) {
+        private double GetDrawdown(List<DatedResult> resultsTw) {
+            if (resultsTw.Any(x => x.Drawdown < 0))
+                return resultsTw.Where(x => x.Drawdown < 0).Average(x => x.Drawdown);
+            else
+                return 0;
+        }
+
+        private void AddRelevantResult(TimeSpan time, Trade t, long i, List<DatedResult> resultsTw) {
+            var relevantResult = t.ResultTimeline.LastOrDefault(x => x.Date < i + time.Ticks);
+            if (relevantResult.Date > 0)
+                resultsTw.Add(relevantResult);
+        }
+
+        private bool CheckRelevant(TimeSpan time, Trade t, long i) {
+            if (t.ResultTimeline.Last().Date < i)
+                return true;
+            if (t.ResultTimeline.First().Date > i + time.Ticks)
+                return true;
+            return false;
+        }
+
+        private List<DatedResult> ParseResults(TimeSpan time, List<Trade> results) {
             Initialise(results);
             for (long i = 0; i < _totalSpan; i += time.Ticks)
                 SumAndAdd(ParseTrades(time, i), i + time.Ticks);
@@ -75,42 +60,74 @@ namespace Thought
             _totalSpan = (_orderedTrades.Last().ResultTimeline.Last().Date - _orderedTrades.First().ResultTimeline.First().Date);
             _categorisedResults = new List<DatedResult>();
         }
+    }
 
-        protected List<DatedResult> ParseTrades(TimeSpan time, long i) {
-            var resultsTw = new List<DatedResult>();
-            foreach (var t in _orderedTrades) {
-                if (CheckRelevant(time, t, i))
-                    continue;
-                AddRelevantResult(time, t, i, resultsTw);
+    public class Backtest
+    {
+        public Universe Markets { get; }
+
+        public Backtest(Universe markets) {
+            Markets = markets;
+        }
+
+        public List<Trade> RunBackTest(StrategyExecuter exec) {
+            var results = new List<Trade>();
+            foreach (var element in Markets.Elements) {
+                exec.Init(element);
+                results.AddRange(exec.ExecuteAll());
             }
-            return resultsTw;
+
+            return results;
         }
 
-        protected void SumAndAdd(List<DatedResult> resultsTw, long date) {
-            var avgDD = GetDrawdown(resultsTw);
-            var sum = resultsTw.Sum(x => x.Return);
-            _categorisedResults.Add(new DatedResult(date, sum, avgDD));
+        public List<Trade> RunBackTestByDates(StrategyExecuter exec ){
+            results = new List<Trade>();
+            counters = new List<int>();
+            finished = new List<bool>();
+            execs = new List<StrategyExecuter>();
+            laggardDate = long.MaxValue;
+            for(int i = 0; i < Markets.Elements.Count ; i++){
+                if(Markets.Elements[i].MarketData.PriceData[0].Open.Ticks < laggardDate)
+                    laggardDate = Markets.Elements[i].MarketData.PriceData[0].Close.Ticks;
+                execs.Add(new LongStrategyExecuter(false));
+                execs[i].Init(Markets.Elements[i]);
+                counters.Add(0);
+                finished.Add(false);
+            }
+            while(!finished.All(x=>x))
+                IterateThroughMarkets();
+            return results;
         }
 
-        protected double GetDrawdown(List<DatedResult> resultsTw) {
-            if (resultsTw.Any(x => x.Drawdown < 0))
-                return resultsTw.Where(x => x.Drawdown < 0).Average(x => x.Drawdown);
-            else
-                return 0;
-        }
+        List<Trade> results {get;set;}
+        List<StrategyExecuter> execs {get;set;}
+        List<int> counters {get;set;}
+        List<bool> finished {get;set; }
+        protected long laggardDate {get;set;}
+        long newlaggardDate {get;set;}
 
-        protected void AddRelevantResult(TimeSpan time, Trade t, long i, List<DatedResult> resultsTw) {
-            var relevantResult = t.ResultTimeline.LastOrDefault(x => x.Date < i + time.Ticks);
-            if (relevantResult.Date > 0)
-                resultsTw.Add(relevantResult);
-        }
+        protected virtual void IterateThroughMarkets(){
+            newlaggardDate = long.MaxValue;
+            for(int i = 0; i < Markets.Elements.Count ; i++){
 
-        protected bool CheckRelevant(TimeSpan time, Trade t, long i) {
-            if (t.ResultTimeline.Last().Date < i)
-                return true;
-            if (t.ResultTimeline.First().Date > i + time.Ticks)
-                return true;
-            return false;
+                if (finished[i] ) 
+                    continue;
+                
+                if (Markets.Elements[i].MarketData.PriceData[counters[i]].Close.Ticks <= laggardDate) {
+                    results.AddRange(execs[i].ExecuteStep());
+                    counters[i]++;
+
+                    if (counters[i] >= Markets.Elements[i].MarketData.PriceData.Length) {
+                        finished[i] = true;
+                        continue;
+                    }
+                }
+
+                if (Markets.Elements[i].MarketData.PriceData[counters[i]].Close.Ticks < newlaggardDate)
+                    newlaggardDate = Markets.Elements[i].MarketData.PriceData[counters[i]].Close.Ticks;
+            }
+            laggardDate = newlaggardDate;
+
         }
     }
 
